0fd5c5e46c646aa2b6b28cdf720a3d4bd13707a9
yanglf <2429445823@qq.com>
2018-05-21 22:15:44 +0800

    第二次提交

--- app/src/main/java/ylf/com/githubtest02/MainActivity.java
+++ app/src/main/java/ylf/com/githubtest02/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         synchronized ("ss"){
-
+            System.out.print("11");
         }
     }
 }
